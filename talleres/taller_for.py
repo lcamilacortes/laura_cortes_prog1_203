699dbe9dc8645b0f8969b6a45f51ef01a86e419c
laura cortes <corteslaura655@gmail.com>
2025-02-27 14:53:43 -0500

    taller for

---
+++ talleres/taller_for.py
@@ -0,0 +1,39 @@
+#rango=range(0,10,2)
+#for i in range(0,10):
+ #   print(i)
+#for i in range(50):
+ #   print(f"carambola numero {i}")
+
+#hacer un ciclo que eleve los numero de 1 a n donde n se ingresa con el usuario
+
+#n=int(input("ingrese numero "))
+#for i in range (1,n):
+    #print (f"{i*i}")
+
+
+
+#imprimir nmultiplos de 3 entre 0 y 100, solicite al usuario un valor de inicio, un valor de terminacion o stop y un 
+# valor de incremento o decremento e imprima el ciclo
+
+
+n=int (input("digite un numero de inicio"))
+j=int (input("digite un valor de terminacion"))
+s= int (input("digite un valor de cuanto en cuanto"))
+
+for i in range (n,j,s):
+    if i%7==0:
+        print(f"son multiplos de 7 {i}")
+    print(i)
+
+    
+
+
+
+
+
+#for i in range (0,100,3):
+   # print (f"{i}")
+
+
+#diga que numeros son multimplos de 7
+
