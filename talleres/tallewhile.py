9efdc95c7cdc25a226171294d32e1ee191f814c2
lcamilacortes <corteslaura655@gmail.com>
2025-02-28 14:32:02 -0500

    ciclos

---
+++ talleres/tallewhile.py
@@ -0,0 +1,14 @@
+#cont=0
+#while True: 
+ #   print("este ciclo es infinito")
+  #  cont+=1
+   # if cont==5:
+    #    break
+
+num=1
+cont=0 
+while num!=0:
+    num=int(input("ingrese numero"))
+    cont+=1
+print (f"usted ingreso {cont} numeros")
+    
