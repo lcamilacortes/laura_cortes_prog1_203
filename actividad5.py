dbd4d7b161386f97d096e7ab867000ef520f9cd8
lcamilacortes <corteslaura655@gmail.com>
2025-02-28 13:01:29 -0500

    nueva clase ciclos

---
+++ actividad5.py
@@ -0,0 +1,3 @@
+ #Solicite un Angulo al usuario en grados. Diga en que cuadrante está. Diga
+#además en que vuelta está sabiendo que cada 360 grados se completa una
+#vuelta a la circunferencia. Además diga el resultado en radianes.
