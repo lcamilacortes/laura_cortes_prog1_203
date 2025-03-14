a1cfca716870d2382ecf9217e0987ade6c0f54cb
lcamilacortes <corteslaura655@gmail.com>
2025-03-14 13:08:59 -0500

    tallerfunciones

---
+++ talleres/funciones/tallerfunciones.py/1.py
@@ -0,0 +1,11 @@
+def fibonnacci(n):
+ a,b=0,1
+ for i in range (n):
+    if n==1:
+        print("0")
+        break
+    if n==2:
+       return f"{n} la serie es"
+    break
+    print(a)
+    a,b= b,a+b
